a57635bccf5a96e393d56c87037352cfdf454109
tangxuandong <jason.tangxd@gmail.com>
2018-11-11 12:34:05 +0800

    add status ext && readme.md

--- txd/stack.py
+++ txd/stack.py
@@ -1,30 +1,40 @@
-
 class stack(object):
     # 栈
     capacity = 10
     top = -1
-    arr = [0]*capacity
+    arr = [0] * capacity
 
     def __init__(self, capacity):
         self.capacity = capacity
         self.top = -1
-        self.arr = [0]*capacity
+        self.arr = [0] * capacity
 
     def push(self, param):
-        self.top = self.top+1
+        self.top = self.top + 1
+        if self.capacity <= self.top:
+            self.extCapacity(capacity=self.capacity)
         self.arr[self.top] = param
 
     def pop(self):
+        if self.top < 0:
+            # raise RuntimeError("已经没有元素可以取了！")
+            return -1
         result = self.arr[self.top]
         self.top = self.top - 1
         return result
 
+    def extCapacity(self, capacity):
+        self.arr = self.arr[:] + [0] * self.capacity
+        self.capacity = capacity * 2
+
 
 if __name__ == '__main__':
-    stack = stack(20)
+    stack = stack(2)
     stack.push(1)
     stack.push(2)
     stack.push(3)
+    print(stack.pop())
+    print(stack.pop())
     print(stack.pop())
     print(stack.pop())
     print(stack.pop())
